5e8cbdb187c912fdf1d1fe7e97de8a07b0977e5d
Itswish <openbookwithyou@gmail.com>
2025-09-16 12:39:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js - Edit these values
 // DEFAULT_M3U8: default stream loaded on page (optional)
-window.DEFAULT_M3U8 = "";
+window.DEFAULT_M3U8 = "https://d14v4v80cpjht7.cloudfront.net/file_library/videos/migration/brightcove/6364382812112/6364382812112.m3u8";
 
 // JOIN_LINKS: up to 4 buttons (title and url)
 window.JOIN_LINKS = [
-  { title: "JOIN CHANNEL 1", url: "https://t.me/your_channel_1" },
-  { title: "JOIN CHANNEL 2", url: "https://t.me/your_channel_2" },
-  { title: "JOIN CHANNEL 3", url: "https://t.me/your_channel_3" },
-  { title: "JOIN CHANNEL 4", url: "https://t.me/your_channel_4" }
+  { title: "JOIN CHANNEL 1", url: "https://t.me/+-c580a3MZ05mNGFl" },
+  { title: "JOIN CHANNEL 2", url: "https://t.me/+-c580a3MZ05mNGFl" },
+  { title: "JOIN CHANNEL 3", url: "https://t.me/+-c580a3MZ05mNGFl" },
+  { title: "JOIN CHANNEL 4", url: "https://t.me/+-c580a3MZ05mNGFl" }
 ];
