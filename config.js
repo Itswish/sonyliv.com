683430aa9d1ed23f0745b9c590528cfa54f63fdf
Itswish <openbookwithyou@gmail.com>
2025-09-16 22:34:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // DEFAULT_M3U8: default stream loaded on page (optional)
-window.DEFAULT_M3U8 = "https://livecfmt-mp.slivcdn.com/v1/manifest/0085f7057986fc2f8467089bb19b8b7e4f1bf8d7/mediaT01adprod-PlaybackConfig/3-c9ccbf62-dc2c-4759-89b4-21b0cd33ba87/3.m3u8";
+window.DEFAULT_M3U8 = "https://livecfmt-mp.slivcdn.com/v1/manifest/0085f7057986fc2f8467089bb19b8b7e4f1bf8d7/mediaT01adprod-PlaybackConfig/3-c9ccbf62-dc2c-4759-89b4-21b0cd33ba87/3.m3u8?hdntl=exp%3D1758077780~acl%3D%2F*~hmac%3D332c5a340346e41d644171cc13a9794e0492f4e49dd4d12838de25b01784926b";
 
 // JOIN_LINKS: up to 4 buttons (title and url)
 window.JOIN_LINKS = [
